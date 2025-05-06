e46bb93445e57164c949a943bbb2ed0cccead71d
Jamyang <tenzinjamyang0901@gmail.com>
2025-05-06 16:07:40 +0200

    added Location options to the addProduct with geolocator and geocoding

--- lib/screens/addProduct.dart
+++ lib/screens/addProduct.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 
@@ -10,10 +12,30 @@ class AddProduct extends StatefulWidget {
 }
 
 class _AddProductState extends State<AddProduct> {
+  final _productTitelController = TextEditingController();
   final _descriptionController = TextEditingController();
   final _priceController = TextEditingController();
-  bool _isAvailable = true;
+  final _streetController = TextEditingController();
+  final _streetNrController = TextEditingController();
+  final _postcodeController = TextEditingController();
+  final _cityController = TextEditingController();
+
+  bool _useCurrentLocation = true;
   File? _image;
+  String? _selectedCategory;
+  double? _latitude;
+  double? _logitude;
+  Position? _location;
+
+  final List<String> _categories = [
+    'Kitchen Appliances',
+    'Cleaning Aplliances',
+    'Tools & DIY',
+    'Laundry & Ironing',
+    'Garden Equipment',
+    'Heating & Cooling',
+    'Other',
+  ];
 
   final ImagePicker _picker = ImagePicker();
 
@@ -26,12 +48,48 @@ class _AddProductState extends State<AddProduct> {
     }
   }
 
-  void _submit() {
+  void _removeImage() {
+    setState(() {
+      _image = null;
+    });
+  }
+
+  Future<void> _getCurrentLocation() async {
+    final permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      await Geolocator.requestPermission();
+    }
+
+    final position = await Geolocator.getCurrentPosition();
+    _latitude = position.latitude;
+    _logitude = position.longitude;
+  }
+
+  Future<void> _getLocationFromAddress() async {
+    final fullAdress =
+        "${_streetController.text},${_streetNrController.text}, ${_postcodeController.text}, ${_cityController.text}";
+    try {
+      List<Location> locations = await locationFromAddress(fullAdress);
+      if (locations.isNotEmpty) {
+        _latitude = locations.first.latitude;
+        _logitude = locations.first.longitude;
+      }
+    } catch (e) {
+      print("Error in geocoding; $e");
+    }
+  }
+
+  void _submit() async {
+    final productTitel = _productTitelController.text.trim();
     final description = _descriptionController.text.trim();
     final priceText = _priceController.text.trim();
     final price = double.tryParse(priceText);
 
-    if (description.isEmpty || price == null || price <= 0 || _image == null) {
+    if (productTitel.isEmpty ||
+        description.isEmpty ||
+        price == null ||
+        price <= 0 ||
+        _image == null) {
       Navigator.of(context).pop();
       Future.delayed(const Duration(milliseconds: 100), () {
         ScaffoldMessenger.of(context).showSnackBar(
@@ -42,23 +100,33 @@ class _AddProductState extends State<AddProduct> {
       });
       return;
     }
+
+    if (!_useCurrentLocation &&
+        (_streetController.text.isEmpty ||
+            _streetNrController.text.isEmpty ||
+            _postcodeController.text.isEmpty ||
+            _cityController.text.isEmpty)) {
+      Navigator.of(context).pop();
+      Future.delayed(const Duration(milliseconds: 100), () {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Please fill in the full address.')),
+        );
+      });
+      return;
+    }
+
+    if (_useCurrentLocation) {
+      await _getCurrentLocation();
+    } else {
+      await _getLocationFromAddress();
+    }
+
     print('submitted');
     Navigator.pop(context);
   }
 
   @override
   Widget build(BuildContext context) {
-    String? _selectedCategory;
-    final List<String> _categories = [
-      'Kitchen Appliances',
-      'Cleaning Aplliances',
-      'Tools & DIY',
-      'Laundry & Ironing',
-      'Garden Equipment',
-      'Heating & Cooling',
-      'Other',
-    ];
-
     return Align(
       alignment: Alignment.topCenter,
       child: Padding(
@@ -72,23 +140,43 @@ class _AddProductState extends State<AddProduct> {
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
               const SizedBox(height: 20),
-              GestureDetector(
-                onTap: _pickImage,
-                child: CircleAvatar(
-                  radius: 60,
-                  backgroundColor: Colors.grey[300],
-                  backgroundImage: _image != null ? FileImage(_image!) : null,
-                  child:
-                      _image == null
-                          ? const Icon(
-                            Icons.camera_alt,
-                            size: 30,
-                            color: Colors.black54,
-                          )
-                          : null,
-                ),
+              Stack(
+                alignment: Alignment.topRight,
+                children: [
+                  GestureDetector(
+                    onTap: _pickImage,
+                    child: CircleAvatar(
+                      radius: 60,
+                      backgroundColor: Colors.grey[300],
+                      backgroundImage:
+                          _image != null ? FileImage(_image!) : null,
+                      child:
+                          _image == null
+                              ? const Icon(
+                                Icons.camera_alt,
+                                size: 30,
+                                color: Colors.black54,
+                              )
+                              : null,
+                    ),
+                  ),
+                  if (_image != null)
+                    IconButton(
+                      onPressed: _removeImage,
+                      icon: const Icon(Icons.delete, color: Colors.red),
+                    ),
+                ],
               ),
               const SizedBox(height: 20),
+              TextField(
+                controller: _productTitelController,
+                decoration: const InputDecoration(
+                  labelText: 'Product Title',
+                  filled: true,
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 10),
               TextField(
                 controller: _descriptionController,
                 decoration: const InputDecoration(
@@ -128,6 +216,77 @@ class _AddProductState extends State<AddProduct> {
                   });
                 },
               ),
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  Expanded(
+                    child: RadioListTile<bool>(
+                      title: const Text('Current Location'),
+                      value: true,
+                      groupValue: _useCurrentLocation,
+                      onChanged:
+                          (val) => setState(() => _useCurrentLocation = val!),
+                    ),
+                  ),
+                  Expanded(
+                    child: RadioListTile<bool>(
+                      title: const Text('Choose Address'),
+                      value: false,
+                      groupValue: _useCurrentLocation,
+                      onChanged:
+                          (val) => setState(() => _useCurrentLocation = val!),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              if (!_useCurrentLocation) ...[
+                Row(
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: TextField(
+                        controller: _streetController,
+                        decoration: const InputDecoration(
+                          labelText: 'Street Name',
+                          filled: true,
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Expanded(
+                      flex: 1,
+                      child: TextField(
+                        controller: _streetNrController,
+                        decoration: const InputDecoration(
+                          labelText: 'Street Number',
+                          filled: true,
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                TextField(
+                  controller: _postcodeController,
+                  decoration: const InputDecoration(
+                    labelText: 'Postcode',
+                    filled: true,
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                TextField(
+                  controller: _cityController,
+                  decoration: const InputDecoration(
+                    labelText: 'City',
+                    filled: true,
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ],
               const SizedBox(height: 20),
               ElevatedButton(
                 onPressed: _submit,
